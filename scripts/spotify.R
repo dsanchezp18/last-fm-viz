41e726039aa24e57708ea4ef9cf23e410730bd8f
Daniel Sánchez Pazmiño <dsanchezp998@gmail.com>
2023-06-05 20:20:28 -0700

    update

--- scripts/spotify.R
+++ scripts/spotify.R
@@ -5,14 +5,16 @@
 library(spotifyr)
 library(tidyverse)
 
+# Set environment variables for spotify (so that the package works)
+
+#Sys.setenv(SPOTIFY_CLIENT_ID = '')
+#Sys.setenv(SPOTIFY_CLIENT_SECRET = '')
+
 # Get the Spotify access token
 
 access_token <- get_spotify_access_token()
 
-
 # Get my all time top tracks
 
-
-top_tracks <-
-  get_my_top_artists_or_tracks(type = 'tracks', time_range = 'long_term', limit = 50) %>% 
-  select(name)
+get_my_top_artists_or_tracks(type = 'artists', limit = 5) %>% 
+  select(name, genres)
